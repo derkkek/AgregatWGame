6b3d0cb76ddce688aba6f6c71bfe0e9ae999b5c4
Halil Eren Öymez <erenhalil2010@hotmail.com>
2026-07-18 21:09:47 +0300

    vertex data correction and blinn-phong shader

--- assets/shaders/default.fs
+++ assets/shaders/default.fs
@@ -9,20 +9,29 @@ uniform vec3 lightColor;
 uniform vec4 colDiffuse;
 uniform sampler2D texture0;
 
-out vec4 finalColor;
+out vec4 FragColor;
+
+uniform vec3 viewPos;
 
 void main()
 {
-    float ambientStrength = 0.1;
-    vec3 ambient = ambientStrength * lightColor;
+    vec3 color = texture(texture0, fragTexCoord).rgb;
 
-    vec3 norm = normalize(fragNormal);
+    vec3 ambient = 0.05 * color;
+    // diffuse
     vec3 lightDir = normalize(lightPos - fragPosition);
-    float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = diff * lightColor;
+    vec3 normal = normalize(fragNormal);
+    float diff = max(dot(lightDir, normal), 0.0);
+    vec3 diffuse = diff * color;
+    // specular
+    vec3 viewDir = normalize(viewPos - fragPosition);
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float spec = 0.0;
 
-    vec4 texelColor = texture(texture0, fragTexCoord);
+    vec3 halfwayDir = normalize(lightDir + viewDir);  
+    spec = pow(max(dot(normal, halfwayDir), 0.0), 32.0);
 
-    vec3 result = (ambient + diffuse) * colDiffuse.rgb * texelColor.rgb;
-    finalColor = vec4(result, colDiffuse.a * texelColor.a);
+
+    vec3 specular = vec3(0.3) * spec; // assuming bright white light color
+    FragColor = vec4(ambient + diffuse + specular, 1.0);
 }
--- src/GraphicsManager/GraphicsManager.cpp
+++ src/GraphicsManager/GraphicsManager.cpp
@@ -16,6 +16,7 @@ namespace Agregat
 
 	void GraphicsManager::Init()
 	{
+		SetConfigFlags(FLAG_MSAA_4X_HINT);
 		InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
 		camera = { 0 };
@@ -28,10 +29,11 @@ namespace Agregat
 		LoadMeshData();
 		engine.resource.LoadShader("shaders/default.vs", "shaders/default.fs", "default");
 		model.materials[0].shader = nameToShaderHashMap["default"];
-
-		lightPos = Vector3{ 0.0f, -3.0f, 0.0f };
+		
+		lightPos = Vector3{ 0.0f, 0.0f, 0.0f };
 
 		DisableCursor();
+		
 		SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
 	}
 	void GraphicsManager::Draw()
@@ -46,14 +48,16 @@ namespace Agregat
 		//DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
 		int lightPos = GetShaderLocation(shader, "lightPos");
 		int lightColor = GetShaderLocation(shader, "lightColor");
+		int viewPos = GetShaderLocation(shader, "viewPos");
 
 		Vector3 lightCol = Vector3{ 1,1,1 };
 		SetShaderValue(shader, lightPos, &this->lightPos, SHADER_UNIFORM_VEC3);
 		SetShaderValue(shader, lightColor, &lightCol, SHADER_UNIFORM_VEC3);
+		SetShaderValue(shader, viewPos, &camera.position, SHADER_UNIFORM_VEC3);
 		
 		BeginMode3D(camera);
 		
-		DrawModelEx(model, Vector3{ 0,0,0 }, Vector3{ 1.0, 0.0, 0.0 }, 180, Vector3{ 1,1,1 }, WHITE);
+		DrawModel(model, Vector3{ 0,0,0 }, 1.0f, WHITE);
 		
 		EndMode3D();
 
@@ -79,6 +83,7 @@ namespace Agregat
 		mesh = engine.resource.LoadMeshFromInterleavedData(planeVertices, vertexCount, 8);
 		model = LoadModelFromMesh(mesh);
 		engine.resource.LoadTexture("textures/wood.png", "wood");
+		SetTextureFilter(nameToTextureHashMap["wood"], TEXTURE_FILTER_BILINEAR);
 		model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = nameToTextureHashMap["wood"];
 	}
 
